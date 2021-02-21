177596d78cee39a9957512a8a6b7ac519b9ebe7f
Kenny Leung <kenny_leung@pobox.com>
2021-02-21 10:12:18 -0800

    Repaired copyright notice on Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,11 @@
 // swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// Copyright (c) 2012-2021 The ANTLR Project. All rights reserved.
+
+// Use of this file is governed by the BSD 3-clause license that		
+// can be found in the LICENSE.txt file in the project root.
+
 import PackageDescription
 
 let package = Package(
